498652ad48ca77be170ba6a1746dffd974ca0dbb
Aleksandr <aleksandrkirilushkin@gmail.com>
2021-12-15 18:43:07 +0300

    feat: add .fxml files and project view

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,24 @@
 package sample;
 
-import sample.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.net.URL;
 
-public class Main {
+public class Main extends Application{
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/sample/Start_window.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
 }
